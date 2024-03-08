a78f4bd2b8f46087b811ede53a8bed5d48250d16
mikekarim07 <135273602+mikekarim07@users.noreply.github.com>
2024-03-07 18:07:19 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+import pickle
+import os
+import datetime
+from io import BytesIO
+import io
+from io import StringIO
+import base64
+import xlsxwriter
+from xlsxwriter import Workbook
+import time
+# import pygwalker as pyg
+# import streamlit.components.v1 as components
+# from pygwalker.api.streamlit import init_streamlit_comm, get_streamlit_html
+
+
+
+st.set_page_config(
+    page_title="Tax Package Model",
+    page_icon="📈",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'mailto:miguel.karim@karimortega.com'
+    }
+)
